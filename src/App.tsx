13ffdd284612f88e5b944b6d5eb27910f30f3544
safciezgi <ezgisafci@outlook.com>
2024-09-13 18:36:08 +0300

    happy-birthday project init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/OIyPoK7aDMTRHIpG/scene.splinecode" />
+  );
+}
+
+
